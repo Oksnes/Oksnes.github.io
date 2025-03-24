df461160e4403d7048b2f0fddd2b8fe92eb2c566
Oksnes <timmen08@outlook.com>
2025-03-24 13:28:59 +0100

    Updated the portfolio ( not finished yet )

--- kode.js
+++ kode.js
@@ -1,24 +1,23 @@
-let button = document.getElementById("specialthing");
+let button = document.getElementById("screenjumper"); // lager ancoren i htmlen til en knapp
 
-let params = "width=495,height=450,left=500,top=200"
-let popup
-button.onclick = () => {
-    popup = window.open("bilder/cat explode.gif", "test", params)
-    
-}
+let params = "width=495,height=450,left=500,top=200"; // hvor stor den skal være og hvor den skal starte
+let popup;
+let condition = true;
 
-setInterval(myTimer,100)
-let condition=true
+button.onclick = () => { // når du trykker på knappen
+    popup = window.open("/bilder/cat explode.gif", "test", params); // lager popupen
+    animatePopup(); // starter animasjonen
+};
 
-document.body.onkeyup = function(e){
-    if(e.keyCode == 32){
-        condition=false
+document.body.onkeyup = function(e) {
+    if (e.keyCode == 32) { // Space bar
+        condition = false; // stopper bevegelsen
     }
-}
+};
 
-function myTimer(){
-
-    if (popup && !popup.closed && condition==true) {
-        popup.moveBy(Math.random() * 100 - 50, Math.random() * 100 - 50);
+function animatePopup() {
+    if (popup && !popup.closed && condition) { // hvis popupen er åpen og bevegelsen ikke er stoppet
+        popup.moveBy(Math.random() * 15 - 6, Math.random() * 15 - 6); // hvor mye den skal bevege seg om gangen
+        requestAnimationFrame(animatePopup); // fortsetter animasjonen
     }
 }
---
+++ portfolio-gammel/kode.js
@@ -0,0 +1,24 @@
+let button = document.getElementById("specialthing");
+
+let params = "width=495,height=450,left=500,top=200"
+let popup
+button.onclick = () => {
+    popup = window.open("../bilder/cat explode.gif", "test", params)
+    
+}
+
+setInterval(myTimer,100)
+let condition=true
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+        condition=false
+    }
+}
+
+function myTimer(){
+
+    if (popup && !popup.closed && condition==true) {
+        popup.moveBy(Math.random() * 100 - 50, Math.random() * 100 - 50);
+    }
+}
