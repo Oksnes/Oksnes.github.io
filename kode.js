d2e73169d8fad311676fbb62751d89b41171d7be
Oksnes <timmen08@outlook.com>
2025-04-30 13:32:48 +0200

    Updating the portfolio a little bit
    +A logo
    +A little bit of a about me
    +Favicon
    +Some renaming of titles

--- kode.js
+++ kode.js
@@ -26,30 +26,30 @@ function animatePopup() {
 //ALT UNDER LAGET AV CHATGPT
 //ALT UNDER LAGET AV CHATGPT
 //ALT UNDER LAGET AV CHATGPT
-const img = document.getElementById('Draggable');
+// const img = document.getElementById('Draggable');
 
-    let offsetX = 0;
-    let offsetY = 0;
-    let isDragging = false;
+//     let offsetX = 0;
+//     let offsetY = 0;
+//     let isDragging = false;
 
-    // When mouse is pressed down on the image
-    img.addEventListener('mousedown', function(event) {
-      isDragging = true;
-      // Calculate the offset from the top-left corner of the image
-      offsetX = event.clientX - img.offsetLeft;
-      offsetY = event.clientY - img.offsetTop;
-    });
+//     // When mouse is pressed down on the image
+//     img.addEventListener('mousedown', function(event) {
+//       isDragging = true;
+//       // Calculate the offset from the top-left corner of the image
+//       offsetX = event.clientX - img.offsetLeft;
+//       offsetY = event.clientY - img.offsetTop;
+//     });
 
-    // When mouse moves
-    document.addEventListener('mousemove', function(event) {
-      if (isDragging) {
-        // Move the image to follow the mouse cursor
-        img.style.left = (event.clientX - offsetX) + 'px';
-        img.style.top = (event.clientY - offsetY) + 'px';
-      }
-    });
+//     // When mouse moves
+//     document.addEventListener('mousemove', function(event) {
+//       if (isDragging) {
+//         // Move the image to follow the mouse cursor
+//         img.style.left = (event.clientX - offsetX) + 'px';
+//         img.style.top = (event.clientY - offsetY) + 'px';
+//       }
+//     });
 
-    // When mouse button is released
-    document.addEventListener('mouseup', function() {
-      isDragging = false;
-    });
+//     // When mouse button is released
+//     document.addEventListener('mouseup', function() {
+//       isDragging = false;
+//     });
